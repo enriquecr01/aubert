f6fb1b739cea5fbd1089f0583d953dc2879a37c0
enriquecr01 <30837445+enriquecr01@users.noreply.github.com>
2022-12-10 15:10:16 -0800

    gosite, username focus, show password methods added, notes field and new words added

--- src/components/password-panel.vue
+++ src/components/password-panel.vue
@@ -8,27 +8,38 @@
     </div>
     <div class="px-10">
         <div class="w-full my-4">
-            <button :class="`${ selectedPassword.color } text-white mr-3 mb-1 px-3 py-2 rounded-full p-ripple`" v-ripple v-tooltip.top="gosite"> <i class=" pi pi-external-link p-button-icon"></i> </button>
+            <button :class="`${ selectedPassword.color } text-white mr-3 mb-1 px-3 py-2 rounded-full p-ripple`" v-ripple v-tooltip.top="gosite" @click="goSite"> <i class=" pi pi-external-link p-button-icon"></i> </button>
             <label class="text-md font-medium text-gray-50">URL </label> 
             <input :class="`w-full bg-gray-600 text-gray-50 px-3 py-2 border border-gray-700 rounded focus:outline-none focus:border-transparent focus:ring-2 focus:${selectedPassword.color.replace('bg', 'ring')} ease-linear transition-all duration-150`" type="text" placeholder="xd.com" :value="selectedPassword.url">
         </div>
 
         <div class="w-full my-4">
-            <button :class="`${ selectedPassword.color } text-white mr-3 mb-1 px-3 py-2 rounded-full p-ripple`"  v-ripple> <i class=" pi pi-user p-button-icon"></i> </button>
-            <label class="text-md font-medium text-gray-50">Username</label>
-            <input :class="`w-full bg-gray-600 text-gray-50 px-3 py-2 border border-gray-700 rounded focus:outline-none focus:border-transparent focus:ring-2 focus:${selectedPassword.color.replace('bg', 'ring')} ease-linear transition-all duration-150`" type="text" placeholder="example@gmail.com" :value="selectedPassword.user">
+            <button :class="`${ selectedPassword.color } text-white mr-3 mb-1 px-3 py-2 rounded-full p-ripple`" @click="focusUsername" v-ripple> <i class=" pi pi-user p-button-icon"></i> </button>
+            <label class="text-md font-medium text-gray-50">{{ usernameLabel }}</label>
+            <input :class="`w-full bg-gray-600 text-gray-50 px-3 py-2 border border-gray-700 rounded focus:outline-none focus:border-transparent focus:ring-2 focus:${selectedPassword.color.replace('bg', 'ring')} ease-linear transition-all duration-150`" type="text" ref="username" placeholder="example@gmail.com" :value="selectedPassword.user">
         </div>
 
         <div class="w-full my-4">
-            <button :class="`${ selectedPassword.color } text-white mr-3 mb-1 px-3 py-2 rounded-full p-ripple`" v-ripple v-tooltip.top="copy"> <i class=" pi pi-copy p-button-icon"></i> </button>
-            <label class="text-md font-medium text-gray-50">Password</label>
+            <button :class="`${ selectedPassword.color } text-white mr-3 mb-1 px-3 py-2 rounded-full p-ripple`" v-ripple v-tooltip.top="copy" @click="copyClipboard"> <i class=" pi pi-copy p-button-icon"></i> </button>
+            <label class="text-md font-medium text-gray-50">{{ passwordLabel }}</label>
 
             <div class="flex justify-end items-center relative">
-                <input :class="`w-full bg-gray-600 text-gray-50 px-3 py-2 pr-10 border border-gray-700 rounded focus:outline-none focus:border-transparent focus:ring-2 focus:${selectedPassword.color.replace('bg', 'ring')} ease-linear transition-all duration-150`" :type="typeTextPassword" placeholder="password" :value="selectedPassword.secret">
-                <i :class="`pi ${visiblePasswordIcon} p-button-icon absolute mr-2 w-6`" @click="passwordVisible"></i>
+                <input :class="`w-full bg-gray-600 text-gray-50 px-3 py-2 pr-14 border border-gray-700 rounded focus:outline-none focus:border-transparent focus:ring-2 focus:${selectedPassword.color.replace('bg', 'ring')} ease-linear transition-all duration-150`" :type="typeTextPassword" placeholder="password" :value="selectedPassword.secret">
+                <div class="absolute inset-y-0 right-0 flex items-center pr-9" v-tooltip.top="{ value: visiblePasswordIcon === 'pi-eye' ? showPassword : hidePassword, class: 'custom-error' }" @click="passwordVisible">
+                    <i :class="`pi ${visiblePasswordIcon} p-button-icon absolute text-gray-50`"></i>
+                </div>
             </div>
                         
         </div>
+
+        <div class="w-full my-4">
+            <button :class="`${ selectedPassword.color } text-white mr-3 mb-1 px-3 py-2 rounded-full p-ripple`" @click="focusUsername" v-ripple> <i class=" pi pi-book p-button-icon"></i> </button>
+            <label class="text-md font-medium text-gray-50">{{ notesLabel }}</label>
+            <textarea rows="4" :class="`w-full bg-gray-600 text-gray-50 px-3 py-2 rounded focus:outline-none focus:border-transparent focus:ring-2 focus:${selectedPassword.color.replace('bg', 'ring')} ease-linear transition-all duration-150`" :placeholder="notesPlaceholder"></textarea>
+        </div>
+
+
+
     </div>
 </template>
 
@@ -50,6 +61,12 @@ export default {
             titleEdit: '',
             gosite: 'no',
             copy: 'no',
+            showPassword: 'no',
+            hidePassword: 'no',
+            notesPlaceholder: 'no',
+            usernameLabel: 'no',
+            passwordLabel: 'no',
+            notesLabel: 'no',
             visiblePasswordIcon: 'pi-eye',
             typeTextPassword: 'password',
         }
@@ -59,6 +76,12 @@ export default {
       this.titleEdit = getWord(this.selectedLang, "titleEdit");
       this.gosite = getWord(this.selectedLang, "gosite");
       this.copy = getWord(this.selectedLang, "copy");
+      this.showPassword = getWord(this.selectedLang, "showPassword");
+      this.hidePassword = getWord(this.selectedLang, "hidePassword");
+      this.notesPlaceholder = getWord(this.selectedLang, "notesPlaceholder");
+      this.usernameLabel = getWord(this.selectedLang, "usernameLabel");
+      this.passwordLabel = getWord(this.selectedLang, "passwordLabel");
+      this.notesLabel = getWord(this.selectedLang, "notesLabel");
     },
     passwordVisible() {
         if (this.visiblePasswordIcon === 'pi-eye') {
@@ -68,7 +91,20 @@ export default {
             this.visiblePasswordIcon = 'pi-eye';
             this.typeTextPassword = 'password';
         }
-    }
+    },
+    goSite() {
+        if (this.selectedPassword.url.includes('https://')) {
+            window.open(this.selectedPassword.url, '_blank').focus();
+        } else {
+            window.open(`https://www.${this.selectedPassword.url}`, '_blank').focus();
+        }
+    },
+    copyClipboard() {
+        navigator.clipboard.writeText(this.selectedPassword.secret);
+    },
+    focusUsername() {
+        this.$refs.username.focus();
+    },
   },
   watch: {
     language(newValue) {
@@ -81,5 +117,8 @@ export default {
 </script>
 
 <style>
-
+    .custom-error .p-tooltip-text {
+        font-size: 0.75rem; /* 12px */
+        line-height: 1rem; /* 16px */
+    }
 </style>
