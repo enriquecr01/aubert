4b0eedb7dcc765e9ec559b666333919a24c38004
Enrique Chavez <30837445+enriquecr01@users.noreply.github.com>
2022-12-31 22:28:38 -0800

    added subroutes and login added

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,21 @@
 <template>
-  <div class="flex flex-wrap w-full h-screen"> 
-
-    <MenuBar @changeLanguage="selectedLang"/>
-    
-    <Menu @changeLanguage="selectedLang" />
-
-
-    <div class="lg:w-10/12 w-full bg-gray-600">
-        <div class="p-4 text-gray-500">
-          <router-view v-slot="{ Component }">
-              <component :is="Component" :language="language" />
-            </router-view>
-        </div>
-    </div>
-
-  </div>
+  <router-view></router-view>
 
 </template>
 
 <script>
-import Menu from './components/menu.vue';
-import MenuBar from './components/menubar.vue';
 
 import './assets/tailwind.css';
 
 export default {
   components: {
-    Menu,
-    MenuBar
   },
   data() {
     return {
-      language: localStorage.getItem('language'),
     }
   },
   methods: {
-    selectedLang(lang) {
-      this.language = lang
-    }
+
   }
 }
 </script>
--- src/router.js
+++ src/router.js
@@ -3,23 +3,32 @@ import Password from './pages/password.vue';
 import Notes from './pages/notes.vue';
 import WiFiPassword from './pages/wifi-password.vue';
 import Login from './pages/login.vue';
-
+import Main from './main.vue';
 
 export const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
-        path: "/",
-        component: Password
-    },{
-        path: "/wifipassword",
-        component: WiFiPassword
-    },{
-        path: "/notes",
-        component: Notes
-    },{
-        path: "/login",
-        component: Login
-    }
+            path: "/",
+            component: Main,
+            children: [
+                {
+                    path: "/",
+                    component: Password
+                },
+                {
+                    path: "/wifipassword",
+                    component: WiFiPassword
+                },
+                {
+                    path: "/notes",
+                    component: Notes
+                },
+            ]
+        },
+        {
+            path: "/login",
+            component: Login
+        }
     ]
 });
